87a48d504de48bbfa3c1eb1aba47307a224036c8
JazyLabs <yassinej@gmail.com>
2018-01-21 18:20:22 +0200

    added simonGame - still needs css final touch

---
+++ simonGame/js/index.js
@@ -0,0 +1,170 @@
+$(document).ready(function() {
+  var steps = [];
+  var userSteps = [];
+  var correct = true;
+  var score = 0;
+  var step = 0;
+  var strict = false;
+  var expert = false;
+  var started = false;
+  var timing = 500;
+  var playTimeOut;
+  function displayMessage(str) {
+    $("#message").html(str);
+  }
+  function playAudio(id) {
+    var audio = document.getElementById(id);
+    if (window.chrome) {
+      audio.load();
+    }
+    audio.play();
+  }
+  function playBtnAudio(id) {
+    var audio = document.getElementById("audio" + id);
+    if (window.chrome) {
+      audio.load();
+    }
+    audio.play();
+  }
+  function start() {
+    started = true;
+    $(".buttons").prop("disabled", true);
+    displayMessage("Let's start");
+    playAudio("audioStart");
+    playTimeOut = setTimeout(function() {
+      play();
+    }, 3000);
+  }
+  function play() {
+    $("#score").html(steps.length + 1);
+    addStep();
+    playStep();
+    step = 0;
+  }
+  function reset() {
+    displayMessage("Let's start from scratch.");
+    steps = [];
+    userSteps = [];
+    score = 0;
+    step = 0;
+    strict = false;
+    expert = false;
+    started = false;
+    timing = 500;
+
+    $("#score").html(0);
+    $("#expert").prop("checked", false);
+    $("#strict").prop("checked", false);
+    $("#0").removeClass("active0");
+    $("#1").removeClass("active1");
+    $("#2").removeClass("active2");
+    $("#3").removeClass("active3");
+    clearTimeout(playTimeOut);
+    playTimeOut = setTimeout(function() {
+      start();
+    }, 2000);
+  }
+  function clickHandler(id) {
+    if (!started) {
+      playBtnAudio(id);
+    } else {
+      playBtnAudio(id);
+      userSteps.push(id);
+      if (userSteps.length === steps.length) {
+        checkSteps();
+      } else {
+        displayMessage("Keep Going..");
+      }
+    }
+  }
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  function addStep() {
+    var i = getRandomInt(4);
+    steps.push(i.toString());
+  }
+  function playStep() {
+    if (step < steps.length) {
+      $("#start").prop("disabled", true);
+      var id = "#" + steps[step];
+      setTimeout(function() {
+        $(id).toggleClass("active" + steps[step]);
+        playBtnAudio(steps[step]);
+      }, timing);
+      setTimeout(function() {
+        $(id).toggleClass("active" + steps[step]);
+        step++;
+        playStep();
+      }, 2 * timing);
+    } else {
+      step = 0;
+      $("#start").prop("disabled", false);
+      $(".buttons").prop("disabled", false);
+    }
+  }
+  function checkSteps() {
+    for (var i = 0; i < userSteps.length; i++) {
+      if (userSteps[i] !== steps[i]) {
+        displayMessage("Try Again..");
+        // playAudio("audioFail");
+        correct = false;
+        userSteps = [];
+        if (strict) {
+          displayMessage("You need more practice..");
+          //playAudio("audioStrictFail");
+          reset();
+          playTimeOut = setTimeout(function() {
+            play();
+          }, 3000);
+        } else {
+          playStep();
+        }
+        return;
+      }
+    }
+    displayMessage("Good! Get Ready for the next step!");
+    userSteps = [];
+    playAudio("audioWin");
+    playTimeOut = setTimeout(function() {
+      play();
+    }, 2000);
+  }
+  function strictMode() {
+    if (!strict) {
+      strict = true;
+      $("#strict").prop("checked", true);
+      displayMessage("Strict Mode Enabled - No Mistakes allowed");
+    } else {
+      strict = false;
+      $("#strict").prop("checked", false);
+      displayMessage("Strict Mode Disabled - Are you a Noob?");
+    }
+  }
+  function expertMode() {
+    if (!expert) {
+      expert = true;
+      timing = 250;
+      $("#expert").prop("checked", true);
+      displayMessage("Export Mode Enabled - Shorter Notifications");
+    } else {
+      expert = false;
+      timing = 500;
+      $("#expert").prop("checked", false);
+      displayMessage("Export Mode Disabled - Longer Notifications");
+    }
+  }
+
+  $("#start").click(function() {
+    start();
+  });
+  $("#reset").click(reset);
+  $("#strict").click(strictMode);
+  $("#expert").click(expertMode);
+  $(".buttons").click(function(event) {
+    clickHandler(event.target.id);
+  });
+  $("#score").html(0);
+
+  console.log("Play Simon game...");
+});
